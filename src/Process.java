d635c5e909932c8c8c1f89a782ee59de2a4a0e24
djordjetodorovic1 <djordje.todorovic04@gmail.com>
2024-12-02 23:21:35 +0100

    Kreiranje klasa

--- src/Process.java
+++ src/Process.java
@@ -1,5 +1,51 @@
 public class Process {
+    private static volatile int counter = 0;
     private int id;
     private ProcessState state;
     private String code;
+    private double burstTime;
+
+    Process(double burstTime) {
+        this.id = counter++;
+        this.state = ProcessState.NEW;
+        this.burstTime = burstTime;
+    }
+
+    Process(double burstTime, String code) {
+        this(burstTime);
+        this.code = code;
+    }
+
+    public void start() {
+        this.state = ProcessState.RUNNING;
+    }
+
+    public void terminate() {
+        this.state = ProcessState.TERMINATED;
+    }
+
+    public void block() {
+        this.state = ProcessState.BLOCKED;
+    }
+
+    public void unblock() {
+        this.state = ProcessState.READY;
+    }
+
+    public ProcessState getState() {
+        return this.state;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public double getBurstTime() {
+        return this.burstTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Process:" + "id=" + this.id + ", state=" + this.state + ", code='" + this.code + '\'' + ", burstTime=" + this.burstTime;
+    }
 }
--- src/ProcessManager.java
+++ src/ProcessManager.java
@@ -1,3 +1,33 @@
-public class ProcessManager {
-    private Process[] processes;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class ProcessManager implements Scheduler {
+    private Queue<Process> pm;
+
+    ProcessManager() {
+        this.pm = new PriorityQueue<>(Comparator.comparingDouble(p->p.getBurstTime()));
+    }
+
+    public synchronized void add(Process p) {
+        pm.offer(p);
+    }
+
+    public synchronized boolean isEmpty() {
+        return pm.isEmpty();
+    }
+
+    @Override
+    public synchronized Process scheduleNextProcess() {
+        return pm.poll();
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (Process p : pm) {
+            result += p + "\n";
+        }
+        return result;
+    }
 }
